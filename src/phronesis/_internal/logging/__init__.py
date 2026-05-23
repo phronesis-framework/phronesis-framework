cf35f2d10bd0f9f43c334ef78efa61775d09ed38
edumarrero <edumarreroglezz@gmail.com>
2026-05-23 16:08:42 +0200

    feat(logging): add package constants

---
+++ src/phronesis/_internal/logging/__init__.py
@@ -0,0 +1,10 @@
+"""Stdlib-based logging: formatters, context adapter, idempotent setup."""
+
+from __future__ import annotations
+
+from .constants import DEFAULT_LEVEL, PHRONESIS_LOGGER_PREFIX
+
+__all__ = [
+    "DEFAULT_LEVEL",
+    "PHRONESIS_LOGGER_PREFIX",
+]
