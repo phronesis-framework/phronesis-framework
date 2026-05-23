3352b375d58bd25d994e7daba729cb1fe7e8acb4
edumarrero <edumarreroglezz@gmail.com>
2026-05-23 16:09:56 +0200

    feat(logging): add idempotent root configuration

--- src/phronesis/_internal/logging/__init__.py
+++ src/phronesis/_internal/logging/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from .adapter import ContextLoggerAdapter
+from .config import configure_logging
 from .constants import DEFAULT_LEVEL, PHRONESIS_LOGGER_PREFIX
 from .factory import get_logger, get_logger_with_context
 from .formatters import HumanReadableFormatter, StructuredFormatter
@@ -13,6 +14,7 @@ __all__ = [
     "ContextLoggerAdapter",
     "HumanReadableFormatter",
     "StructuredFormatter",
+    "configure_logging",
     "get_logger",
     "get_logger_with_context",
 ]
