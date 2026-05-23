ec5f7aa767a39c0f5ce3d5ef71742ec711042c95
edumarrero <edumarreroglezz@gmail.com>
2026-05-23 16:09:32 +0200

    feat(logging): add logger factory helpers

--- src/phronesis/_internal/logging/__init__.py
+++ src/phronesis/_internal/logging/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from .adapter import ContextLoggerAdapter
 from .constants import DEFAULT_LEVEL, PHRONESIS_LOGGER_PREFIX
+from .factory import get_logger, get_logger_with_context
 from .formatters import HumanReadableFormatter, StructuredFormatter
 
 __all__ = [
@@ -12,4 +13,6 @@ __all__ = [
     "ContextLoggerAdapter",
     "HumanReadableFormatter",
     "StructuredFormatter",
+    "get_logger",
+    "get_logger_with_context",
 ]
